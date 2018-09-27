e2e373073316d6b04c2abd7319fb5e59dd9812a8
Bruno Scoti <bruno.scoti@yahoo.com.br>
2018-09-27 19:11:09 -0300

    terceiro commit, modificação no layout

---
+++ client/main.js
@@ -0,0 +1,30 @@
+
+
+Template.main.events({
+    /*'focus #divHeader input'(event, instance) {
+        if(document.querySelector('.focus')){
+            document.querySelector('.focus').classList.remove('focus');
+
+        }
+        document.getElementById('divHeader').classList.add('focus');
+        
+            
+    },
+    'focusout #divHeader input'(event,instance){
+        
+    },'focus #divQuestion'(event, instance) {
+        if(document.querySelector('.focus')){
+            document.querySelector('.focus').classList.remove('focus');
+
+        }
+        document.getElementById('divQuestion').classList.add('focus');
+        
+    },
+    'focusout #divQuestion'(event,instance){
+        
+    }*/
+
+    
+  
+  });
+  
